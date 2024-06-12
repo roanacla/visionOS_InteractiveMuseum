c758d01d2f384ce8678066eed3137737db8919ba
Roger Navarro <roanacla@me.com>
2024-06-12 00:15:26 -0700

    Add Tap Gesture to select, then add close button to move item back to its original position

--- InteractiveMuseum/ImmersiveView.swift
+++ InteractiveMuseum/ImmersiveView.swift
@@ -10,8 +10,12 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    @State var isObjectSelected = false
+    @State var currentEntity: Entity?
+    @State var previousPosition: SIMD3<Float> = [0,0,0]
+    
     var body: some View {
-        RealityView { content in
+        RealityView { content, attachments in
             // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
@@ -25,10 +29,66 @@ struct ImmersiveView: View {
                 // Put skybox here.  See example in World project available at
                 // https://developer.apple.com/
             }
-        } update: { content in
-            //
+        } update: { content, attachments in
+            if let attachmentEntity = attachments.entity(for: "h1") {
+                content.add(attachmentEntity)
+//                currentEntity?.addChild(attachmentEntity)
+                attachmentEntity.setPosition([0,-10,10], relativeTo: currentEntity)
+            }
+        } attachments: {
+            if isObjectSelected {
+                Attachment(id: "h1") {
+                    HStack {
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Scale")
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Rotate")
+                        })
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.5))
+                    .cornerRadius(10)
+                    
+                    VStack {
+                        Button(action: {
+                            currentEntity?.setPosition(previousPosition, relativeTo: nil)
+                            currentEntity = nil
+                            previousPosition = [0,0,0]
+                            isObjectSelected = false
+                        }, label: {
+                            Text("Close")
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Rotate")
+                        })
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.5))
+                    .cornerRadius(10)
+                }
+            }
         }
         .installGestures()
+        .gesture(tapGesture)
+    }
+    
+    var tapGesture: some Gesture {
+        TapGesture()
+            .targetedToAnyEntity()
+            .onEnded { value in
+                isObjectSelected = true
+                currentEntity = value.entity
+                previousPosition = value.entity.position
+                value.entity.setPosition([0, 1.5, -1], relativeTo: nil)
+                // position object 0.5 meters in front of user.
+            }
     }
 }
 
